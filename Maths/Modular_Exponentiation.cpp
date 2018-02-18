cd9411b0db7e045c1bfe606d82450a030ab8492b
arnab17 <thearvancouple@gmail.com>
2018-02-18 10:36:52 +0530

    Create Modular_Exponentiation.cpp

---
+++ Maths/Modular_Exponentiation.cpp
@@ -0,0 +1,13 @@
+ll modular_pow(ll base, ll exponent)
+{
+    ll result = 1;
+    while (exponent > 0)
+    {
+        if (exponent % 2 == 1)
+            result = (result * base) % mod;
+        exponent = exponent >> 1;
+        base = (base * base) % mod;
+    }
+    return result;
+}
+ 
