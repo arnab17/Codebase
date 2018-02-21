1b509d50b91f17b4fbd8c2dba0444af16cac7e12
arnab17 <thearvancouple@gmail.com>
2018-02-21 19:36:33 +0530

    Create comparing_doubles.cpp

---
+++ Maths/comparing_doubles.cpp
@@ -0,0 +1,20 @@
+// Check if two doubles are same
+
+bool AreSame(double a, double b)
+{
+    return fabs(a - b) < EPSILON;
+}
+
+// Definitely Greater Than 
+
+bool definitelyGreaterThan(float a, float b, float epsilon)
+{
+    return (a - b) > ( (fabs(a) < fabs(b) ? fabs(b) : fabs(a)) * epsilon);
+}
+
+// Definitely Less Than
+
+bool definitelyLessThan(float a, float b, float epsilon)
+{
+    return (b - a) > ( (fabs(a) < fabs(b) ? fabs(b) : fabs(a)) * epsilon);
+}
