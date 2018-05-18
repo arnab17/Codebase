46c7edddfc2a5892608b3972626d134224c0e0f7
arnab17 <thearvancouple@gmail.com>
2018-05-18 19:20:15 +0530

    Update CBARG.cpp

--- Contests/JUNE15/CBARG.cpp
+++ Contests/JUNE15/CBARG.cpp
@@ -1,1 +1,26 @@
+// https://www.codechef.com/JUNE15/problems/CBARG
 
+#include<bits/stdc++.h>
+using namespace std;
+
+#define MAX 100009
+#define ll long long
+
+int arr[MAX];
+
+int main(){
+	int n,t,i;
+	scanf("%d",&t);
+	while(t--){
+		scanf("%d",&n);
+		for(i=0;i<n;++i) scanf("%d",&arr[i]);
+		ll ans=(ll)arr[0];
+		for(i=1;i<n;++i){
+			if(arr[i]>arr[i-1]){
+				ans=ans+((ll)arr[i]-(ll)arr[i-1]);
+			}
+		}
+		printf("%lld\n",ans);
+	}
+	return 0;
+}
